2661f0f0179289b8220d038f72faf7f59b11e281
Samuel Javelle <samueljavelle@hotmail.com>
2023-04-23 22:27:14 +0200

    V2 avec tous les accents

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+//cacher l'input avec autofocus
+document.getElementById("mytext").focus();
+document.addEventListener('click', () => {
+    document.getElementById("mytext").focus();
+})
+
 // Globals
 let nbrGuesses = 6;
 let nextLetter = 0;
 let rightGuessString = '';
-
 const board = document.querySelector('#game-board');
 const date = new Date()
-document.querySelector('#gameId').textContent = moment(date).format('DD/MM/YYYY')
+
 
 //un mot par jour
+document.querySelector('#gameId').textContent = moment(date).format('DD/MM/YYYY')
 let seed = moment(date).format('DDMMYYYY')
-//let seed = 123
 let x = Math.sin(seed++) * 10000;
 let randomindex = Math.floor( ( x - Math.floor(x) ) * dico.length );
 rightGuessString = dico[randomindex];
 //console.log(rightGuessString)
 
-//generate board
+//generate game board
 for (let i = 0; i < nbrGuesses; i++) {
     board.innerHTML += `<div class="letter-row"></div>`;
     for (let j = 0; j < rightGuessString.length; j++) {
@@ -29,192 +34,185 @@ document.querySelector('#newGame').addEventListener('click', function(){
 })
 
 ///GAME
-document.addEventListener('keyup', function(event){
-    if(event.key === 'Backspace'){
-        const lastLetter = document.querySelectorAll('.filled-box:not(.tested)')
-        lastLetter[lastLetter.length-1].textContent = ''
-        lastLetter[lastLetter.length-1].className = "letter-box"
-        nextLetter -= 1
+let string = '';
+let eventKey = '';
+document.getElementById("mytext").addEventListener('keyup', function (event) {
+    if(this.value.length <= rightGuessString.length){
+        string = this.value
+        eventKey = event.key
 
-    }else if(event.key === 'Enter'){
-        if(nextLetter === rightGuessString.length){
-            //check le mot
-            const rightGuessArray = rightGuessString.split('')
-            let rightGuessArrayTest = rightGuessArray
-            const letters = document.querySelectorAll('.filled-box:not(.tested)')
+        /// rentrer les lettres
+        let places = document.querySelectorAll('.letter-box:not(.filled-box)')
+        for (let i=0; i<rightGuessString.length ;i++){
+            places[i].textContent = string[i]
+        }
 
-            let test = [];
-            let testword = [];
-            for(let i=0; i< rightGuessArray.length; i++){
-                test.push(letters[i].textContent)
-            }
+        if(eventKey === 'Enter'){
+            if(string.length === rightGuessString.length){
+                for (let i=0; i<rightGuessString.length ;i++){
+                    places[i].className = "letter-box filled-box"
+                }
+                this.value = '';
+                let previous = document.querySelectorAll(".filled-box:not(.tested)")
+                if(dico.some(e => e === string)){
+                    ///mot existant
+                    if(string === rightGuessString){
+                        ///mot gagnant
+                        for(let i=0; i< previous.length; i++){
+                            previous[i].className += " won"
+                        }
 
-            if( dico.some(e => e === test.join('') ) ){
-                //les gagnants
-                for(let i=0; i< rightGuessArray.length; i++){
-                    testword.push(letters[i].textContent)
-                    if(testword[i] === rightGuessArray[i] ) {
-                        letters[i].className = "letter-box filled-box tested won"
-                        rightGuessArrayTest = rightGuessArrayTest.filter(e => e !== testword[i])
-                        //console.log(rightGuessArrayTest)
+                        document.querySelector('#title').textContent = `motus!`
+
+                        const sleep = (time) => {
+                            return new Promise((resolve) => setTimeout(resolve, time))
+                        }
+                        const animation = async () => {
+                            for (let i = 0; i < rightGuessString.length; i++) {
+                                await sleep(200)
+                                setTimeout(() => {
+                                    previous[i].className = "letter-box filled-box tested loose"
+                                }, 200);
+                                setTimeout(() => {
+                                    previous[i].className = "letter-box filled-box tested won"
+                                }, 400);
+                                setTimeout(() => {
+                                    previous[i].className = "letter-box filled-box tested loose"
+                                }, 600);
+                                setTimeout(() => {
+                                    previous[i].className = "letter-box filled-box tested won"
+                                }, 800);
+                                setTimeout(() => {
+                                    previous[i].className = "letter-box filled-box tested loose"
+                                }, 1000);
+                                setTimeout(() => {
+                                    previous[i].className = "letter-box filled-box tested won"
+                                }, 1200);
+                            }
+                        }
+                        animation()
+        
+                        let audio = new Audio("sound/win.mp3");
+                        audio.play();
+
+                    /// test des lettres
+                    }else{
+                        let testString =rightGuessString;
+                        for (let i=0; i<string.length; i++){
+                            //bonnes lettres
+                            if(string[i] === rightGuessString[i]){
+                                previous[i].className += " tested won"
+                                //combien de i dans testletter?
+                                let j = testString.indexOf(string[i])
+                                testString = testString.substring(0,j) + testString.substring(j+1,testString.length)
+                            //lettres inclues
+                            }else if(testString.includes(string[i])){
+                                previous[i].className += " tested near"
+                                let j = testString.indexOf(string[i])
+                                testString = testString.substring(0,j) + testString.substring(j+1,testString.length)
+                            //lettres fausses
+                            }else{
+                                previous[i].className += " tested loose"
+                            }
+                        }
                     }
-                }
-
-                //les proches
-                testword = [];
-                for(let i=0; i< rightGuessArray.length; i++){
-                    testword.push(letters[i].textContent)
-                    console.log(testword)
-                    if(rightGuessArrayTest.includes(testword[i])){
-                        //console.log(`${testword[i]} n'est pas à la bonne place`)
-                        letters[i].className = "letter-box filled-box tested near"
-                        rightGuessArrayTest = rightGuessArrayTest.filter(e => e !== testword[i])
+                }else{
+                    /// mot inexistant
+                    for(let i=0; i< previous.length; i++){
+                        previous[i].className += " tested invalid"
                     }
+                    document.querySelector('#title').textContent = `c'est quoi ce mot?`
+                    setTimeout(() => {
+                        document.querySelector('#title').textContent = 'bouche cousue...'
+                    }, 2000);
                 }
 
-                //les fausses
-                const loose = document.querySelectorAll('.filled-box:not(.tested)')
-                for (let i=0; i<loose.length; i++){
-                    loose[i].className = "letter-box filled-box tested loose"
-                }
-
-            }else {
-                document.querySelector('#title').textContent = "mot invalide"
-                for(let i=0; i< rightGuessArray.length; i++){
-                    letters[i].className = "letter-box filled-box tested invalid"
-                }
+            /// pas assez de lettres pour tester
+            }else{
+                document.querySelector('#title').textContent = `on a dit ${rightGuessString.length} lettres!`
                 setTimeout(() => {
                     document.querySelector('#title').textContent = 'bouche cousue...'
-                }, 3000);
-            }
-
-            //mot gagnant
-            if(testword.join('') === rightGuessArray.join('')){
-                document.querySelector('#title').textContent = "motus!"
-
-                const sleep = (time) => {
-                    return new Promise((resolve) => setTimeout(resolve, time))
-                }
-                const animation = async () => {
-                    for (let i = 0; i < rightGuessArray.length; i++) {
-                      await sleep(200)
-                      setTimeout(() => {
-                          letters[i].className = "letter-box filled-box tested loose"
-                      }, 200);
-                      setTimeout(() => {
-                          letters[i].className = "letter-box filled-box tested won"
-                      }, 400);
-                      setTimeout(() => {
-                          letters[i].className = "letter-box filled-box tested loose"
-                      }, 600);
-                      setTimeout(() => {
-                          letters[i].className = "letter-box filled-box tested won"
-                      }, 800);
-                      setTimeout(() => {
-                          letters[i].className = "letter-box filled-box tested loose"
-                      }, 1000);
-                      setTimeout(() => {
-                          letters[i].className = "letter-box filled-box tested won"
-                      }, 1200);
-                    }
-                }
-                animation()
-
-                let audio = new Audio("sound/win.mp3");
-                audio.play();
-                return
-            
-            //game over
-            }else if(document.querySelectorAll('.tested').length === rightGuessString.length*nbrGuesses){
-                document.querySelector('#title').textContent = "game over"
-                let gameOver = document.querySelectorAll('.tested')
-
-                const sleep = (time) => {
-                    return new Promise((resolve) => setTimeout(resolve, time))
-                }
-                const animation = async () => {
-                    for (let i = 0; i < gameOver.length; i++) {
-                        await sleep(30)
-                        if(i % 2 === 0) {
-                            gameOver[i].className = 'letter-box filled-box tested gameover'
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested loose'
-                            }, 100);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested gameover'
-                            }, 200);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested loose'
-                            }, 300);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested gameover'
-                            }, 400);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested loose'
-                            }, 500);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested gameover'
-                            }, 600);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested loose'
-                            }, 700);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested gameover'
-                            }, 800);
-                        }else{
-                            gameOver[i].className = 'letter-box filled-box tested loose'
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested gameover'
-                            }, 100);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested loose'
-                            }, 200);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested gameover'
-                            }, 300);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested loose'
-                            }, 400);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested gameover'
-                            }, 500);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested loose'
-                            }, 600);
-                            setTimeout(() => {
-                                gameOver[i].className = 'letter-box filled-box tested gameover'
-                            }, 700);
-                        }   
-                    }
-                }
-                animation()
-
-                let audio = new Audio("sound/loose.mp3");
-                audio.play();
-                return
-            }
-
-            nextLetter = 0
-
-        }else{
-            document.querySelector('#title').textContent = "ça manque de lettres.."
-        }
-
-    }else if(event.key.match(/^[a-zA-ZÀ-ÿ]$/)){
-
-        if(document.querySelector('#title').textContent === "ça manque de lettres.."){
-            document.querySelector('#title').textContent = "bouche cousue..."
-        }
-
-        for(let i=0; i<rightGuessString.length; i++){
-            if(nextLetter < i+1){
-                document.querySelector('.letter-box:not(.filled-box)').textContent = event.key
-                document.querySelector('.letter-box:not(.filled-box)').className = "letter-box filled-box"
-                nextLetter += 1
-                break
+                }, 2000);
             }
         }
-
-    }
     
-})
+    /// empêcher la saise de plus de lettres que le mot du jour
+    }else{
+        this.value = this.value.slice( 0, rightGuessString.length )
+        string = this.value
+    }
+    //game over
+    if(document.querySelectorAll('.tested').length === rightGuessString.length*nbrGuesses){
+        document.querySelector('#title').textContent = "game over"
+
+        let gameOver = document.querySelectorAll('.tested')
+        const sleep = (time) => {
+            return new Promise((resolve) => setTimeout(resolve, time))
+        }
+        const animation = async () => {
+            for (let i = 0; i < gameOver.length; i++) {
+                await sleep(30)
+                if(i % 2 === 0) {
+                    gameOver[i].className = 'letter-box filled-box tested gameover'
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested loose'
+                    }, 100);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested gameover'
+                    }, 200);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested loose'
+                    }, 300);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested gameover'
+                    }, 400);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested loose'
+                    }, 500);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested gameover'
+                    }, 600);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested loose'
+                    }, 700);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested gameover'
+                    }, 800);
+                }else{
+                    gameOver[i].className = 'letter-box filled-box tested loose'
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested gameover'
+                    }, 100);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested loose'
+                    }, 200);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested gameover'
+                    }, 300);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested loose'
+                    }, 400);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested gameover'
+                    }, 500);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested loose'
+                    }, 600);
+                    setTimeout(() => {
+                        gameOver[i].className = 'letter-box filled-box tested gameover'
+                    }, 700);
+                }   
+            }
+        }
+        animation()
+
+        let audio = new Audio("sound/loose.mp3");
+        audio.play();
+    }
+});
+
+    
+
+// TO-DO
+// gérer les accent circonflexes
+// gérer un clavier mobile
